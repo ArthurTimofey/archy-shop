10257e9d9cd71fd28d9e579ef0b59ccf8611ca51
ArtursTimofejevs <arturs@sixty.no>
2023-04-18 15:45:13 +0100

    made nav DRY

--- src/lib/components/Nav.svelte
+++ src/lib/components/Nav.svelte
@@ -1,56 +1,29 @@
-<header aria-label="Site Header" class="bg-white">
+<script lang="ts">
+	export let title: string;
+	export let links: { href: string; label: string }[];
+</script>
+
+<header aria-label="Site Header" class="bg-white border-b-teal-400 border-b-4">
 	<div class="mx-auto flex h-16 max-w-screen-xl items-center gap-8 px-4 sm:px-6 lg:px-8">
 		<a class="block text-teal-600" href="/">
 			<span class="sr-only">Home</span>
-			<h1>Archy</h1>
+			<h1 class="font-bold">{title}</h1>
 		</a>
 
-		<div class="flex flex-1 items-center justify-end md:justify-between">
+		<div class="flex flex-1 items-center justify-end">
 			<nav aria-label="Site Nav" class="hidden md:block">
 				<ul class="flex items-center gap-6 text-sm">
-					<li>
-						<a class="text-gray-500 transition hover:text-gray-500/75" href="/"> About </a>
-					</li>
-
-					<li>
-						<a class="text-gray-500 transition hover:text-gray-500/75" href="/"> Careers </a>
-					</li>
-
-					<li>
-						<a class="text-gray-500 transition hover:text-gray-500/75" href="/"> History </a>
-					</li>
-
-					<li>
-						<a class="text-gray-500 transition hover:text-gray-500/75" href="/"> Services </a>
-					</li>
-
-					<li>
-						<a class="text-gray-500 transition hover:text-gray-500/75" href="/"> Projects </a>
-					</li>
-
-					<li>
-						<a class="text-gray-500 transition hover:text-gray-500/75" href="/"> Blog </a>
-					</li>
+					{#each links as link}
+						<li>
+							<a class="text-gray-500 transition hover:text-gray-500/75" href="{link.href}">
+								{link.label}</a
+							>
+						</li>
+					{/each}
 				</ul>
 			</nav>
 
 			<div class="flex items-center gap-4">
-				<div class="sm:flex sm:gap-4">
-					<a
-						class="block rounded-md bg-teal-600 px-5 py-2.5 text-sm font-medium text-white transition hover:bg-teal-700"
-						href="/"
-					>
-						Login
-					</a>
-
-					<a
-						class="hidden rounded-md bg-gray-100 px-5 py-2.5 text-sm font-medium text-teal-600 transition hover:text-teal-600/75 sm:block"
-						href="/"
-					>
-						Register
-					</a>
-				</div>
-
 				<button
 					class="block rounded bg-gray-100 p-2.5 text-gray-600 transition hover:text-gray-600/75 md:hidden"
 				>
@@ -63,7 +36,7 @@
 						stroke="currentColor"
 						stroke-width="2"
 					>
-						<path stroke-linecap="round" stroke-linejoin="round" d="M4 6h16M4 12h16M4 18h16" />
+						<path stroke-linecap="round" stroke-linejoin="round" d="M4 6h16M4 12h16M4 18h16"></path>
 					</svg>
 				</button>
 			</div>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,13 @@
 <script lang="ts">
 	import '../app.postcss';
 	import { Nav } from '$lib/components';
+
+	const title = 'Archy Shop';
+	const links = [
+		{ href: '/', label: 'Shop' },
+		{ href: '/contact', label: 'Contact' }
+	];
 </script>
 
-<Nav />
+<Nav title="{title}" links="{links}" />
 <slot />
